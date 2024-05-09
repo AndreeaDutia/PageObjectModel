ff58986c7a774c3837b01af4142cbbb3ed8a989e
Andreea Dutia <andreea.balu@gmail.com>
2024-05-09 11:30:04 +0300

    curs18 final

---
+++ src/main/java/tests/LoginTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import pages.MenuPage;
+import utils.BaseTest;
+import utils.PropertiesFileProcessor;
+
+public class LoginTest extends BaseTest {
+	
+	String user = PropertiesFileProcessor.readPropertiesFile("username", "credentials.properties");
+	String pass = PropertiesFileProcessor.readPropertiesFile("password", "credentials.properties");
+	
+	@Test(priority=1)
+	public void validLoginTest() {
+		
+		MenuPage menu = new MenuPage(driver);
+		LoginPage login = new LoginPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		login.loginInApp(user, pass);
+		
+		assertTrue(login.checkMsgIsDisplayed(login.successLoginMessage));
+		login.logoutFromApp();
+		
+	}
+	
+	@Test(priority=2)
+	public void invalidLoginTest() {
+		
+		MenuPage menu = new MenuPage(driver);
+		LoginPage login = new LoginPage(driver);
+		
+		menu.navigateTo(menu.loginLink);
+		login.loginInApp(user, "123456");
+		
+		assertTrue(login.checkMsgIsDisplayed(login.errorLoginMessage));
+		//assertFalse(login.checkMsgIsDisplayed(login.successLoginMessage));
+	}
+
+}
---
+++ src/main/java/utils/PropertiesFileProcessor.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesFileProcessor {
+//public class PropertiesFileProcessor (String key, String value) {
+	
+	
+	
+	//read properties file
+	public static String readPropertiesFile(String key, String file) {
+		try (InputStream inputStream = new FileInputStream(file)) {
+			
+			Properties propFile = new Properties();
+			propFile.load(inputStream);
+			return propFile.getProperty(key);
+			
+			
+		}catch(IOException e) {
+			System.out.println("Nu am putut citi fisierul!");
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+}
