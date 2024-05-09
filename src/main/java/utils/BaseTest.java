ca65509ab774d63b0a354c3245435801ca9caf8d
Andreea Dutia <andreea.balu@gmail.com>
2024-05-09 09:42:20 +0300

    curs18

---
+++ src/main/java/utils/BaseTest.java
@@ -0,0 +1,31 @@
+package utils;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+	
+	public WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://keybooks.ro/");
+		
+		}
+	
+	@AfterClass
+	public void tearDown() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.quit(); //inchide toate taburile
+	//	driver.close();//inchide tabul curent
+	}
+
+}
