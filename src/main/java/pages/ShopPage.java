128aea0753ec71bfc93fd3e8598ca8428ab2477e
Andreea Dutia <andreea.balu@gmail.com>
2024-05-20 11:27:04 +0300

    curs21final

--- src/main/java/pages/ShopPage.java
+++ src/main/java/pages/ShopPage.java
@@ -3,6 +3,7 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 public class ShopPage {
@@ -15,8 +16,16 @@ public class ShopPage {
 	
 	//locatori
 	public By orderDropdown = By.name("orderby");
+	public By sliderInitialPosition = By.xpath("//span[@style='left: 0%;']");
+	public By sliderFinalPosition = By.xpath("//span[@style='left: 100%;']");
 	
-	
+	public void dragAndDrop(By locator, int x, int y) {
+		WebElement element = driver.findElement(locator);
+		Actions action = new Actions(driver);
+		//action.dragAndDropBy(element, x, y).perform();
+		action.moveToElement(element).clickAndHold(element).moveByOffset(x, y).release().perform();
+		
+	}
 	//metode
 	public void filterByValue(String value) {
 		WebElement element = driver.findElement(orderDropdown);
