7c6ed4c430ea8ad76325091a495d1dd0f8ef7cea
Andreea Dutia <andreea.balu@gmail.com>
2024-06-02 16:49:31 +0300

    tema23

--- src/main/java/tests/JSExecutorTema.java
+++ src/main/java/tests/JSExecutorTema.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import pages.MenuPage;
 import seleniumutils.BaseTest;
 
 public class JSExecutorTema extends BaseTest {
@@ -13,24 +14,42 @@ public class JSExecutorTema extends BaseTest {
 	public void example1() throws InterruptedException {
 		JavascriptExecutor jse = (JavascriptExecutor)driver;
 		
+		
+		String javaScriptHover = "var objObject = document.createEvent('MouseEvents');" +  "objObject.initMouseEvent('mouseover',true);" + "arguments[0].dispatchEvent(objObject);";
+		
 		WebElement Blog = driver.findElement(By.id("menu-item-76"));
-		WebElement PostFormats = driver.findElement(By.id("menu-item-114"));
+		WebElement PostFormats = driver.findElement(By.cssSelector("a[href*='post-formats']"));
 		 
-        jse.executeScript("arguments[0].click()", Blog);
-        Thread.sleep(3000);
+        jse.executeScript(javaScriptHover, Blog);
+        
         jse.executeScript("arguments[0].click()", PostFormats);
-
-        String javaScriptHover = "var objObject = document.createEvent('MouseEvents');" + "objObject.initMouseEvent('mouseover',true);" + "arguments[0].dispatchEvent(objObject);";
+        Thread.sleep(3000);
+       
+        /*
+        WebElement galleryFormat = driver.findElement(By.cssSelector("a[href*='gallery-format']"));
+        jse.executeScript("arguments[0].scrollIntoView()", galleryFormat);
+		jse.executeScript("arguments[0].click()", galleryFormat);
+		
+		WebElement comment = driver.findElement(By.cssSelector("textarea[id=comment]"));
+        jse.executeScript("arguments[0].scrollIntoView()", comment);
+        jse.executeScript("arguments[0].value='comentariu'", comment);
         
-		/*jse.executeScript("document.getElementsByClassName('sf-with-ul').click()");
-		Thread.sleep(3000);
-		jse.executeScript("document.getElementById('menu-item-114').click()");
-		Thread.sleep(3000);
-		*/
+        WebElement nume = driver.findElement(By.cssSelector("input[id=author]"));
+        jse.executeScript("arguments[0].value='nume'", nume);
         
-		String pageURl = jse.executeScript("return document.URL").toString();
-		System.out.println(pageURl);
-		System.out.println(driver.getCurrentUrl());
+        WebElement email = driver.findElement(By.cssSelector("input[id=email]"));
+        jse.executeScript("arguments[0].value='test@test.ro'", email);
+        
+        WebElement website = driver.findElement(By.cssSelector("input[id=url]"));
+        jse.executeScript("arguments[0].value='www.test.ro'", website);
+        
+        WebElement postCommentButton = driver.findElement(By.cssSelector("input[id=send_comment]"));
+		jse.executeScript("arguments[0].click()", postCommentButton);
+        */
+        
+        jse.executeScript("document.getElementsByClassName('gallery-format')[0].scrollIntoView()");
+		
+		
 		
 	}
 
